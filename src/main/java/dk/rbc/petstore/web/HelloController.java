fffa573ec5b27d8cf9fcc0a72788107679f14dba
daaa57150 <daaa57150@hotmail.com>
2016-10-11 18:22:07 +0200

    add sources

---
+++ src/main/java/dk/rbc/petstore/web/HelloController.java
@@ -0,0 +1,26 @@
+package dk.rbc.petstore.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+   
+    @Value("${test.name}")
+    private String name;
+    
+    @RequestMapping("/")
+    public String index() {
+        
+        String msg = "Greetings from Spring Boot " + name + " !"; 
+        LOGGER.debug(msg);
+        
+        return msg;
+    }
+
+}
